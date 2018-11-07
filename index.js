b105a9f33d74f629c72564586a27abd2935d8161
Alexis Rico <SferaDev@users.noreply.github.com>
2018-11-07 23:40:31 +0100

    Add more tags from dox
    * Add more tags from dox
    
    * Enable multiline quotes on description

--- index.js
+++ index.js
@@ -59,22 +59,22 @@ const parser = (content, filename) =>
         .map(method => ({
             'uid': formatStringForUID(`${filename}-${method.ctx.string}`),
             'isPrivate': method.isPrivate,
+            'isClass': method.isClass,
+            'isConstructor': method.isConstructor,
             'type': method.ctx.type,
             'name': formatStringForName(method.ctx.string),
-            'description': method.description.full,
+            'description': method.description.full.replace(/\n/g, '<br/>'),
             'empty': !method.description.full && !method.tags.length,
+            'code': method.code,
+            'commentLine': method.line,
+            'codeLine': method.codeStart,
             'params': method.tags.filter(tag =>
                 tag.type === 'param' && !tag.name.match(/\./))
                 .map(tag => {
-
                     if (tag.optional) {
-
                         return `[${formatStringForParam(tag.name)}]`;
-
                     }
-
                     return formatStringForParam(tag.name);
-
                 })
                 .join(', ')
                 .replace(/\], \[/g, ', ')
